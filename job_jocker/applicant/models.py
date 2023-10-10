ed5730009baf9b53f1e655d07547cbed43debd88
anastasia <moiri@mail.ru>
2023-10-10 18:53:00 +0200

    Applicants basics

--- job_jocker/applicant/models.py
+++ job_jocker/applicant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+#import user from user_app 
+import uuid
 
-# Create your models here.
+class Resume(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    #user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100)
+    skills = models.TextField(null=True, blank=True)
+    education = models.CharField(max_length=100)
+    salary = models.IntegerField(default=0, null=True, blank=True)
+    photo = models.ImageField(
+        null=True, blank=True, upload_to='profile_images', default="profile_images/default.png")
+    is_posted = models.BooleanField(default=False, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+        return f"{self.surname} {self.profession}"
--- job_jocker/applicant/views.py
+++ job_jocker/applicant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Resume
 
-# Create your views here.
+def resumes(request):
+    resumes = Resume.objects.all()
+    context = {'resumes': resumes}
+    return render(request, 'resumes.html', context)
+
+def resume(request, pk):
+    resumeObj = Resume.objects.get(id=pk)
+    return render(request, 'one_resume.html', {'resume': resumeObj})
