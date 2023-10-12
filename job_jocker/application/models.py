bad24ae6b94c1976d4a33d53cfabec6452021d39
anastasia <moiri@mail.ru>
2023-10-13 00:10:42 +0200

    Application basics added

--- job_jocker/application/models.py
+++ job_jocker/application/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+#import user from user_app
+from django.contrib.auth.models import User
+from employer.models import Vacancy, Card
+from applicant.models import Resume
 
-# Create your models here.
+
+class Application(models.Model):
+    resume_id = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    vacancy_id = models.ForeignKey(Vacancy, on_delete=models.CASCADE)
+    resume_id = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    creator_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    response = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        # return f"{Resume.objects.get(id=self.resume_id).profession}"
+        return f"{Resume.objects.get(id=self.resume_id.id).profession}"
--- job_jocker/job_jocker/urls.py
+++ job_jocker/job_jocker/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applicant.urls')),
     path('employer/', include('employer.urls')),
+    path('application/', include('application.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
